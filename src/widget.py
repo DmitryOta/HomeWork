1bcef60a51f29a2f31099e08bc18f666d6c38983
Dmitry_X <dmitrydsm@bk.ru>
2024-08-04 22:39:58 +0300

    add two func

---
+++ src/widget.py
@@ -0,0 +1,21 @@
+import masks
+
+
+def mask_account_card(cards_accounts: str) -> str:
+    """Функция принимает номер карты или счета и возвращает их маску"""
+    mask_cards_account = []
+    split_cards_accounts = cards_accounts.split()
+    for velue in split_cards_accounts:
+        if velue.isalpha():
+            mask_cards_account.append(velue)
+        if velue.isdigit() and len(velue) == 16:
+            mask_cards_account.append(masks.get_mask_card_number(velue))
+        if velue.isdigit() and len(velue) > 16:
+            mask_cards_account.append(masks.get_mask_account(velue))
+    return " ".join(mask_cards_account)
+
+
+def get_date(date: str) -> str:
+    """Функция принемает дату и возвращает ее в формате ДД.ММ.ГГГГ"""
+    reverse_date = ".".join(date[:10].split("-")[::-1])
+    return reverse_date
