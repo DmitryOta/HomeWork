d57742b201e46258aa975976b2deb59d6fb2edc3
Dmitry_X <dmitrydsm@bk.ru>
2024-08-20 19:21:59 +0300

    fix tets_decorators.py

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,6 +1,26 @@
 from src.decorators import log
 
 
+@log()
+def sum_num(a, b, c):
+    return a + b + c
+
+
+def test_log_complete(capsys):
+    sum_num(10, 15, 20)
+    captured = capsys.readouterr()
+    assert captured.out == "Старт функции sum_num\nsum_num завершена успешно\n"
+
+
+def test_log_error(capsys):
+    sum_num(10, 15, "10")
+    captured = capsys.readouterr()
+    assert (
+        captured.out
+        == """Старт функции sum_num
+sum_num error: тип ошибки. unsupported operand type(s) for +: 'int' and 'str'\n"""
+    )
+
 
 def test_log():
     @log()
@@ -9,22 +29,3 @@ def test_log():
 
     result = sum_num(10, 15, 20)
     assert result == 45
-
-
-@log()
-def sum_num(a, b, c):
-    return a + b + c
-
-def test_log_capsus(capsys):
-    sum_num(10, 15, 20)
-    captured = capsys.readouterr()
-    assert captured.out == 'Старт функции sum_num\nsum_num завершена успешно\n'
-
-
-def test_log_capsus(capsys):
-    sum_num(10, 15, "10")
-    captured = capsys.readouterr()
-    assert captured.out == """Старт функции sum_num
-sum_num error: тип ошибки. unsupported operand type(s) for +: 'int' and 'str'\n"""
-
-
