e4327ce6555b12eaed042e37c8141a8e54a014c1
Dmitry_X <dmitrydsm@bk.ru>
2024-08-20 17:50:09 +0300

    add func log

---
+++ src/decorators.py
@@ -0,0 +1,22 @@
+from typing import Any, Callable
+
+
+def log(filename: str ="") -> Callable:
+    def wrapper(func: Callable) -> Callable:
+        def inner(*args: Any, **kwargs: Any) -> Any:
+            print(f"Старт функции {func.__name__}")
+            try:
+                result = func(*args, **kwargs)
+                log_result = f"{func.__name__} завершена успешно"
+            except Exception as e:
+                log_result = f"{func.__name__} error: тип ошибки. {e}"
+                result = None
+            finally:
+                if not filename:
+                    print(log_result)
+                else:
+                    with open(filename, "a") as file:
+                        file.write(log_result + "\n")
+            return result
+        return inner
+    return wrapper
