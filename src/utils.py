ed6a188be7442a8f424792406641388a8e732718
Dmitry_X <dmitrydsm@bk.ru>
2024-08-28 18:41:22 +0300

    add func unpacking_json

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+WEY_TO_FILE = os.path.join(os.path.dirname(__file__), "../data/operations.json")
+
+
+def unpacking_json(WEY_TO_FILE: str) -> list[dict]:
+    operation_list: list = []
+    with open(WEY_TO_FILE, encoding="utf-8") as json_file:
+        try:
+            data = json.load(json_file)
+            if type(data) is not list:
+                return operation_list
+            else:
+                return data
+        except FileNotFoundError:
+            return operation_list
+        except json.JSONDecodeError:
+            return operation_list
