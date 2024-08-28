77bc4b580b00b8c266f7dd59ca4fa90d39a77eec
Dmitry_X <dmitrydsm@bk.ru>
2024-08-28 20:18:38 +0300

    dell fixtere, add docstring unpacking_json

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ WEY_TO_FILE = os.path.join(os.path.dirname(__file__), "../data/operations.json")
 
 
 def unpacking_json(WEY_TO_FILE: str) -> list[dict]:
+    """Функция, принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях"""
     operation_list: list = []
     with open(WEY_TO_FILE, encoding="utf-8") as json_file:
         try:
--- tests/conftest.py
+++ tests/conftest.py
@@ -60,8 +60,3 @@ def input_transactions():
             "to": "Счет 14211924144426031657",
         },
     ]
-
-
-@pytest.fixture
-def input_json():
-    return '[{"name": "Dmitry", "age": 30, "profession": "Phyton"}]'
