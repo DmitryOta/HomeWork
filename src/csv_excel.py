9a29d467fcad476033a9a64816ff2431754211fa
Dmitry_X <dmitrydsm@bk.ru>
2024-09-09 11:54:26 +0300

    add file test_csv_excel.py

--- src/csv_excel.py
+++ src/csv_excel.py
@@ -1,15 +1,17 @@
-import os
-import pandas as pd
 import csv
+import os
+
+import pandas as pd
+
+WEY_TO_FILE_CSV: str = os.path.join(os.path.dirname(__file__), "../csv_excel/transactions.csv")
+
+WEY_TO_FILE_EXCEL: str = os.path.join(os.path.dirname(__file__), "../csv_excel/transactions_excel.xlsx")
 
 
-WEY_TO_FILE_CSV = os.path.join(os.path.dirname(__file__), "../csv_excel/transactions.csv")
-
-WEY_TO_FILE_EXCEL = os.path.join(os.path.dirname(__file__), "../csv_excel/transactions_excel.xlsx")
-
-def reading_financial_transactions_csv(WEY_TO_FILE_CSV: str) -> list[str]:
+def reading_financial_transactions_csv(WEY_TO_FILE_CSV: str) -> list[dict]:
+    """Функция считывает финансовые операции из CSV файла и фозвращает список словарей с транзакциями"""
     with open(WEY_TO_FILE_CSV, encoding="utf-8") as file:
-        reader = csv.DictReader(file, delimiter=';')
+        reader = csv.DictReader(file, delimiter=";")
         new_list = []
         for row in reader:
             new_list.append(row)
@@ -17,8 +19,6 @@ def reading_financial_transactions_csv(WEY_TO_FILE_CSV: str) -> list[str]:
 
 
 def reading_financial_transactions_excel(WEY_TO_FILE_EXCEL: str) -> list[dict]:
+    """Функция считывает финансовые операции из xlsx файла и фозвращает список словарей с транзакциями"""
     df = pd.read_excel(WEY_TO_FILE_EXCEL)
     return df.to_dict(orient="records")
-
-
-print(reading_financial_transactions_csv(WEY_TO_FILE_CSV))
---
+++ tests/test_csv_excel.py
@@ -0,0 +1,71 @@
+import csv
+import os
+from unittest.mock import Mock
+
+from src.csv_excel import WEY_TO_FILE_CSV, reading_financial_transactions_csv, reading_financial_transactions_excel
+
+wey_to_excel = os.path.join(os.path.dirname(__file__), "../csv_excel/test_Excel.xlsx")
+
+
+def test_reading_financial_transactions_csv():
+    mock_transaction = Mock(
+        return_value=[
+            {
+                "id": "650703",
+                "state": "EXECUTED",
+                "date": "2023-09-05T11:30:32Z",
+                "amount": "16210",
+                "currency_name": "Sol",
+                "currency_code": "PEN",
+                "from": "Счет 58803664561298323391",
+                "to": "Счет 39745660563456619397",
+                "description": "Перевод организации",
+            }
+        ]
+    )
+    csv.DictReader = mock_transaction
+    assert reading_financial_transactions_csv(WEY_TO_FILE_CSV) == [
+        {
+            "id": "650703",
+            "state": "EXECUTED",
+            "date": "2023-09-05T11:30:32Z",
+            "amount": "16210",
+            "currency_name": "Sol",
+            "currency_code": "PEN",
+            "from": "Счет 58803664561298323391",
+            "to": "Счет 39745660563456619397",
+            "description": "Перевод организации",
+        }
+    ]
+
+
+def test_reading_financial_transactions_excel():
+    mock_transaction = Mock(
+        return_value=[
+            {
+                "id": "650703",
+                "state": "EXECUTED",
+                "date": "2023-09-05T11:30:32Z",
+                "amount": "16210",
+                "currency_name": "Sol",
+                "currency_code": "PEN",
+                "from": "Счет 58803664561298323391",
+                "to": "Счет 39745660563456619397",
+                "description": "Перевод организации",
+            }
+        ]
+    )
+    csv.DictReader = mock_transaction
+    assert reading_financial_transactions_excel(wey_to_excel) == [
+        {
+            "id": 650703.0,
+            "state": "EXECUTED",
+            "date": "2023-09-05T11:30:32Z",
+            "amount": 16210.0,
+            "currency_name": "Sol",
+            "currency_code": "PEN",
+            "from": "Счет 58803664561298323391",
+            "to": "Счет 39745660563456619397",
+            "description": "Перевод организации",
+        }
+    ]
